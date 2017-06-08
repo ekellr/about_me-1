766448f57874843085cd3e75e08169578720efb3
dmuldrew <dmuldrew@gmail.com>
2017-06-08 01:18:04 -0700

    rewrite w/o functions

--- js/app.js
+++ js/app.js
@@ -1,71 +1,87 @@
-'use strict'
+'use strict';
 
-// This was abandoned because of the potential for a question/answer array mismatches
-// var questions = [
-//                   "Does he sail?",
-//                   "Can he juggle?",
-//                   "Is he a morning person?",
-//                   "Is he from Seattle?",
-//                   "Does he play the cello?"
-//                 ];
-//
-// var correctAnswerArray = ["yes","no","no","no","n0"];
+var questionData = [
+                    ["yesNo", "Does he sail?", "yes", 1];
+                    ["yesNo", "Can he juggle?", "no", 1];
+                    ["yesNo", "Is he a morning person?", "no", 1];
+                    ["yesNo", "Is he from Seattle?", "no", 1];
+                    ["yesNo", "Does he play the cello?", "no", 1];
+                    ["interval", "Does he play the cello?", "no", 4];
+                    ["multiple", "Does he play the cello?", "no", 6];
+                  ];
 
-// Decided that a dictionary with questions as keys, and the question answer
-// as the associated value would be easier to manage
-var questionsDict = {
-                      "Does he sail?":"yes",
-                      "Can he juggle?":"no",
-                      "Is he a night person?":"yes",
-                      "Did he grow up in Seattle?":"no",
-                      "Does he play the cello?":"no"
-                    };
-// console.log(questionsDict);
+// console.log(questionsData);
 
 // Initialization variables to accumulate results
 var answersArray = [];
 var scoreArray = [];
 var scoreTotal = 0;
 
-// This function takes in a question and prompts the user for an answer
+// This loop takes a question and prompts the user for an answer
 // It then calls another function to check if the user answer is correct
 // Finally, it pushes the user answer and score to arrays, and adds to score total
-var theQuestioner = function (question)
+for (i = 0; i < questionsData.length; i++)
   {
-    var answer = prompt(question + ' yes or no (y or n)');
-    var score = theValidator(answer.toLowerCase(), question);
+    var questionInfo = questionData[1];
+
+    var questionType = questionInfo[0];
+    var question = questionInfo[1];
+    var rightAnswer = questionInfo[2];
+    var numTries = questionInfo[3];
+
+    var score;
+
+    console.log(question);
+
+    if questionType === 'yesNo'){
+      var answer = prompt(question[1] + ' yes or no (y or n)').toLowerCase();
+
+      if (answer) {
+        // Either the first letter or full answer - can be a mix or lower and upper case.
+        var answerMatchBool = ((answer === rightAnswer[0]) || (answer === rightAnswer));
+        console.log(answer);
+        console.log(answerMatchBool);
+      }
+
+    else if (questionType === 'interval') {
+      for (var i = numTries; i >= 0; i--){
+        var answer = prompt(question[1] + ' You get 4 tries.');
+
+        if () {
+
+        } else if {
+
+        } else if {
+
+        } else {
+
+        }
+      }
+
+    }
+
+    else if (questionType === 'multiple') {
+      for (var i = numTries]; i >= 0; i--){
+
+        var answer = prompt(question[1] + ' Choose among these options: ' + rightAnswer);
+
+
+        
+      }
+    }
+
+    if (answerMatchBool){
+      score = 1;
+    } elseif {
+      score = 0;
+    }
 
     answersArray.push(answer);
     scoreArray.push(score);
     scoreTotal += score;
   };
-
-// This takes the users answer/current question and computes a score
-var theValidator = function (answer, question)
-    {
-      if (answer) {
-        // looks up right answer to the question
-        var rightAnswer = questionsDict[question];
-        // Either the first letter or full answer - can be a mix or lower and upper case.
-        var answerMatchBool = ((answer === rightAnswer[0]) || (answer === rightAnswer));
-        console.log(question);
-        console.log(answer);
-        console.log(answerMatchBool);
-        if (answerMatchBool){
-          return 1;
-        }
-      }
-      return 0;
-    };
-
-// returns an array of keys for dictionary
-var questionsArray = Object.keys(questionsDict);
-// calls theQuestioner function for each item in array
-questionsArray.forEach(theQuestioner);
-
+}
 // prints final results
-console.log('These are the questions with answers:');
-console.log(questionsDict);
 console.log('These are your answers: ' + answersArray);
 console.log('These are your points for each question: ' + scoreArray);
 console.log('This is your score total: ' + scoreTotal);
